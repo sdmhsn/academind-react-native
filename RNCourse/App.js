cb9e4ff868d89612e5db4d454fa4d02c25a56236
Saddam Husein <saddambinhusein2@gmail.com>
2025-12-21 23:41:13 +0800

    Splitting components into smaller components

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -29,13 +37,10 @@ export default function App() {
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
         <FlatList
-          data={courseGoal} renderItem={(itemData) => {
+          data={courseGoal}
+          renderItem={(itemData) => {
             // { console.log(itemData); }
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem />;
           }}
           keyExtractor={(item, index) => {
             // { console.log(item); }
@@ -43,7 +48,7 @@ export default function App() {
           }}
         />
       </View>
-    </View >
+    </View>
   );
 }
 
@@ -60,25 +65,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
+    borderBottomColor: '#cccccc',
   },
   TextInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
     width: '70%',
     marginRight: 8,
-    padding: 8
+    padding: 8,
   },
   goalsContainer: {
     flex: 5,
   },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white'
-  }
 });
