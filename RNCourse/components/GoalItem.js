38620e9a8131a0f8425bf5115d54607636600ae9
Saddam Husein <saddambinhusein2@gmail.com>
2025-12-23 00:37:19 +0800

    Adding an android_ripple effect

--- RNCourse/components/GoalItem.js
+++ RNCourse/components/GoalItem.js
@@ -3,11 +3,14 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 function GoalItem(props) {
   // console.log(props);
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#25064eff', foreground: true }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -16,11 +19,11 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
   goalText: {
     color: 'white',
+    padding: 8,
   },
 });
